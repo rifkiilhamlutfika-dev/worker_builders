21abd79ac58252c41100312940da62794dfc405d
rifkiilhamlutfika-dev <rifkiilhamlutfika.dev@gmail.com>
2025-05-30 22:22:20 +0700

    PROGRES : Setup react, inertia & shadcn ui

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/app', function () {
+    return Inertia::render('Test');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Test');
 });
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwind from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.jsx"],
             refresh: true,
         }),
-        tailwindcss(),
+        tailwind(),
+        react(),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+        },
+    },
 });
